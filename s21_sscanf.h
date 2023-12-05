ecedc0b12f60307451bfd80b2e2f455a0f35fad1
BulatRuslanovich <bulatruslanovich@gmail.com>
2023-12-05 21:53:46 +0300

    add char and string

--- s21_sscanf.h
+++ s21_sscanf.h
@@ -1,34 +1,48 @@
 #ifndef SRC_S21_SSCANF_H_
 #define SRC_S21_SSCANF_H_
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>  // потом свою подключем
+#define BUFF_SIZE 512
 
-typedef enum widthType { NONE_WIDTH,
-                       WIDTH_NUMBER,
-                       WIDTH_STAR } widthType;
+typedef enum widthType { NONE_WIDTH, WIDTH_NUMBER, WIDTH_STAR } widthType;
 
 enum errorCode {
-	OK = -1,
-	ERROR,
+  ERROR = -1,
+  OK,
 };
 
 typedef enum lengthType {
-    NONE_LENGTH,
-    LENGTH_SHORT,
-    LENGTH_LONG,
-    LENGTH_LONG_LONG,
-    LENGTH_DOUBLE,
-    LENGTH_LONG_DOUBLE
+  NONE_LENGTH,
+  LENGTH_SHORT,
+  LENGTH_LONG,
+  LENGTH_LONG_LONG,
+  LENGTH_DOUBLE,
+  LENGTH_LONG_DOUBLE
 } lengthType;
 
 typedef struct token {
-    void *address;  
-    widthType widthType;
-	lengthType lengthType;
-    unsigned int width;
-    char buffer[512];
-    char spec;
+  void *address;
+  widthType widthType;
+  lengthType lengthType;
+  unsigned int width;
+  char buffer[512];
+  char spec;
 } token;
 
 int s21_sscanf(const char *str, const char *format, ...);
-
-
-#endif  
+int isSpace(char c);
+int isLetter(char c);
+int isDigit(char c);
+int checkEOFString(const char *str);
+token parseToken(char **formatPtr, va_list *va);
+void skipSpaces(char **str);
+void parseWidth(char **formatPtr, token *result);
+int parseNumber(char **formatPtr);
+void parseLength(char **formatPtr, token *result);
+void parseSpecifier(char **formatPtr, token *result);
+int writeTokensToMemory(char **strPtr, token *tokens, int tokenLen);
+int writeCharToMem(char **str, token *tok);
+int writeStringToMem(char **str, token *tok);
+#endif
